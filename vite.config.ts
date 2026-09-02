8cc314e35ede265331a2c30ece385a66185f524d
Jasonpan711 <jason@novisenai.com>
2026-09-02 15:27:25 +0800

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base:'/PM-Fitline/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
